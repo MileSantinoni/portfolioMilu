1d6947b519a2271fa0fc69911ca396c7eaea22b8
Milu Santinoni <santinonimilena@gmail.com>
2023-09-27 21:46:45 -0300

    sección de proyectos agregada

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ let navlist = document.querySelector('.navlist');
 let nombre = prompt("¿Cómo te llamas?");
 
 const h5 = document.querySelector('h5');
-h5.innerHTML = "¡Hola"+" "+ nombre + "!";
+ h5.innerHTML = "¡Hola"+" "+ nombre + "!";
 
 
 
 const sr = ScrollReveal ({
     distance: '65px',
     duration: 2600,
-    delay: 450,
+    delay: 300,
     reset: true
 });
 
@@ -21,7 +21,12 @@ sr.reveal('.scroll-down', {delay:500, origin: 'right'});
 
 
 
-sr.reveal('.tecnos', {delay:200, origin: 'top'});
-sr.reveal('.img-tecnos', {delay:450, origin: 'top'});
-sr.reveal('.img-tecnos-text', {delay:500, origin: 'left'});
-sr.reveal('.contenedor', {delay:500, origin: 'buttom'});
+sr.reveal('.tecnos', {delay:50, origin: 'top'});
+sr.reveal('.img-tecnos', {delay:300, origin: 'top'});
+sr.reveal('.img-container', {delay:350, origin: 'left'});
+sr.reveal('.contenedor', {delay:350, origin: 'buttom'});
+
+sr.reveal('.proyectos', {delay:50, origin: 'top'});
+sr.reveal('.card', {delay:300, origin: 'top'});
+
+
